18b4bbd42e06777af3f5a50d0666b62c0f8503fc
Gustafa Bayu <faafa13@gmail.com>
2023-03-13 15:36:28 +0700

    spring boot with mongodb

---
+++ src/test/java/com/barito/springcrudmongodb/SpringCrudMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barito.springcrudmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
